fdd089a92bd8beda684018ef478bb4434526d0cd
Alex Hoppe <32897621+amhoppe@users.noreply.github.com>
2018-02-02 15:10:39 -0500

    Comments on Script file
    Added comments to the javascript file

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ $(document).ready(function() {
 
 $(".row").css("display", "block");
 $(".search-results").css("display", "none");
-
+// Initializes all the variables and logs testing page
 console.log("testing page");
 
 var Lat;
@@ -35,6 +35,8 @@ var city;
 var state;
 function yelp(data){};
 
+
+// accepts the user's input and splits the string into useable data for API's State, City
 $("#submit-button").on("click", function(event) {
   console.log("button clicked");
   event.preventDefault();
@@ -49,6 +51,9 @@ $("#submit-button").on("click", function(event) {
   var address = inputResults.split(",");
   state = address[1];
   city = address[0];
+
+
+  //Below this line are all the Query URL's for the API's we are using.  
     var queryURL =
     "http://api.wunderground.com/api/a4c1cc1f438c8eaf/conditions/q/"+ state + "/" + city +".json";
   // "http://api.wunderground.com/api/a4c1cc1f438c8eaf/conditions/q/NC/raleigh.json"
@@ -57,10 +62,16 @@ $("#submit-button").on("click", function(event) {
   //http://api.waqi.info/search/?token="+token()+"&keyword="+keyword  
     var queryURLyelpr
     var queryURLschool = "https://api.schooldigger.com/v1.1/schools?st="+ state +"&city="+ city+ "&perPage=2&sortBy=rank&appID=7c44cfcc&appKey=53c1651cdc939f078f21cf3bdaf98356";
-   
+   //https://api.schooldigger.com/v1.1/schools?st="+ state +"&city="+ city+ "&perPage=2&sortBy=rank&appID=&appKey=
+
+//Console Logging all the query urls
 console.log(queryURL);
 console.log(queryURLair);
 console.log(queryURLyelpr);
+
+
+// This is our first API call for weather underground.  This API is important because it gives both the weather, temp, precipitaion, and the Location in Longitude and Latitude
+// I used success on this call to link to the function yelp.  The Yelp API requires longitude and latitude as parameters so the weather API must run first.
 
   $.ajax({
     url:queryURL,
@@ -73,14 +84,24 @@ console.log(queryURLyelpr);
 
     $("#testing").append(response);
 
+    // Here is function yelp. It logs the latitude and longitude as variables and creates the queryURL's
     function yelp(){
       Lat = response.current_observation.display_location.latitude;
       Long = response.current_observation.display_location.longitude;
+
+      // There will need to be a different queryURL for each different business users wish to see.  queryURLyelpr is for resturants 
        var queryURLyelpr =
     "https://api.yelp.com/v3/businesses/search?term=resturants&latitude="+Lat+"&longtitude="+Long+"&radius=10000&categories=restaurants&limit=1&sort_by=rating&attributes=hot_and_new"
+       
+      // Commented out this yelp would be for bars
        //var queryURLyelpb =
     //"https://api.yelp.com/v3/businesses/search?term=bar" 
+    
+
     console.log(queryURLyelpr);
+    
+      // This AJAX call is used to get the data from yelp.  It has to have authorization in the header.  Review the documentation
+      // Currently this call does not show up with errors or with data.  Very Confusing.  
 
     $.ajax({
       url: queryURLyelpr,
@@ -99,6 +120,8 @@ console.log(queryURLyelpr);
 
   });
 
+// This AJAX call is for the Air Quality of the City.  It also has a widget that is not operational in the html
+
   $.ajax({
       url:queryURLair,
       method: "GET"
@@ -107,6 +130,9 @@ console.log(queryURLyelpr);
       var air = response;
       console.log(air);
     });
+
+//  This AJAX call is for the school data in a city.  How the query URL is set up currently is to return the top two ranked schools in the city.  
+//  This AjAX call is currently throwing an error 400
 
   $.ajax({
     url:queryURLschool,
